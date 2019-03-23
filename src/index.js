2015caa2baa6818907328516c38788ff896da9f2
Olga Bogatyreva <helga_bogatyreva@mail.ru>
2019-03-23 20:04:53 +0300

    feat: add the method makeExchange

--- src/index.js
+++ src/index.js
@@ -1,5 +1,37 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
+  let num = parseInt(currency);
+  if (num === 0) {
+    return {};
+  }
+
+  if (currency > 10000) {
+    return {
+      error: "You are rich, my friend! We don't have so much coins for exchange"
+    };
+  }
+
+  let division = function(val, by){
+    return (val - val % by) / by;
+  }
+  let temp = {};
+  let arr = {
+    H : 50,
+    Q : 25,
+    D : 10,
+    N : 5,
+    P : 1
+  };
+  for (let k in arr) {
+    let div = division (currency, arr[k]);
+    if (div > 0) {
+      temp[k]= div;
+      currency = currency - div * arr[k];
+    }
+  }
+  return temp;
+
     // Your code goes here!
     // Return an object containing the minimum number of coins needed to make change
 }
+
